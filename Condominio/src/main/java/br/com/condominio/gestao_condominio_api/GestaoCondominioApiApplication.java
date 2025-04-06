8bc99d40d017c524b7c99aabfc81850e7d69e053
andrerosa98 <20242012000024@pucgo.edu.br>
2025-04-06 19:56:10 -0300

    Commit inicial, setup do front e backend com Spring Boot e React

---
+++ Condominio/src/main/java/br/com/condominio/gestao_condominio_api/GestaoCondominioApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.condominio.gestao_condominio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoCondominioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoCondominioApiApplication.class, args);
+	}
+
+}
---
+++ Condominio/src/test/java/br/com/condominio/gestao_condominio_api/GestaoCondominioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.condominio.gestao_condominio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoCondominioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
