91187886e27b8fb8d71110a78086dd24ea547f60
André Luis Ferreira da Rosa Junior <20242012000024@pucgo.edu.br>
2025-05-17 20:26:08 -0300

    criação do cadastro de usuário v1.0

--- src/main/java/com/gestaoCondominio/CadastroUsuario.java
+++ src/main/java/com/gestaoCondominio/CadastroUsuario.java
@@ -5,8 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class CadastroUsuario {
-    public static void cadastrarUsuario(String nome, String email, String senha) {
-        String sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
+    public static void cadastrarUsuario(String nome, String email, String senha, String cpf, String dataNascimento) {
+        String sql = "INSERT INTO usuarios (nome, email, senha, cpf, data_nascimento) VALUES (?, ?, ?, ?, ?)";
 
         try (Connection conexao = ConexaoBD.getConexao();
              PreparedStatement stmt = conexao.prepareStatement(sql)) {
@@ -14,11 +14,13 @@ public class CadastroUsuario {
             stmt.setString(1, nome);
             stmt.setString(2, email);
             stmt.setString(3, senha);
+            stmt.setString(4, cpf);
+            stmt.setString(5, dataNascimento);
 
             stmt.executeUpdate();
             System.out.println("Usuário cadastrado com sucesso!");
-        } catch (SQLException e) {
-            System.err.println("Erro ao cadastrar usuário: " + e.getMessage());
+        } catch (SQLException erro) {
+            System.err.println("Erro ao cadastrar usuário: " + erro.getMessage());
         }
     }
 }
--- src/main/java/com/gestaoCondominio/Main.java
+++ src/main/java/com/gestaoCondominio/Main.java
@@ -1,7 +1,23 @@
 package main.java.com.gestaoCondominio;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Cadastro de Usuário");
+        System.out.print("Nome: ");
+        String nome = input.nextLine();
+        System.out.print("Email: ");
+        String email = input.nextLine();
+        System.out.print("Senha: ");
+        String senha = input.nextLine();
+        System.out.print("CPF (Somente números: ");
+        String cpf = input.nextLine();
+        System.out.print("Data de Nascimento (YYYY-MM-DD): ");
+        String dataNascimento = input.nextLine();
+
+        CadastroUsuario.cadastrarUsuario(nome, email, senha, cpf, dataNascimento);
     }
 }
