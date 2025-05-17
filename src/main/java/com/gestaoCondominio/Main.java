87ded83031fe7e56d27d459c8af25f39bf04df3b
André Luis Ferreira da Rosa Junior <20242012000024@pucgo.edu.br>
2025-05-17 19:54:03 -0300

    Criação do arquivo sql para criar o BD do programa

--- src/main/java/com/gestaoCondominio/Main.java
+++ src/main/java/com/gestaoCondominio/Main.java
@@ -2,6 +2,6 @@ package main.java.com.gestaoCondominio;
 
 public class Main {
     public static void main(String[] args) {
-        CadastroUsuario.cadastrarUsuario("João Silva", "joao.silva@example.com", "senha123");
+        System.out.print("Hello, World!");
     }
 }
