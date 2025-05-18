d14a674364b38ebb503ae0516ac942bed60f67b8
André Luis Ferreira da Rosa Junior <20242012000024@pucgo.edu.br>
2025-05-18 15:40:44 -0300

    Criação do loop para criação de usuários e organização do menu no Main

---
+++ src/com/gestaoCondominio/service/Menu.java
@@ -0,0 +1,16 @@
+package com.gestaoCondominio.service;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void inicial(){
+        System.out.println("------------------------");
+        System.out.println("Sistema GestCondo");
+        System.out.println("Menu");
+        System.out.println("1 - Cadastrar Usuário");
+        System.out.println("0 - Sair");
+        System.out.print("Digite a opção desejada: ");
+    }
+
+}
